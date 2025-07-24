0dd4c174be5309fef8643a03435a227e521f2aa6
Mansoor Sahito <mansoorsahito601@gmail.com>
2025-07-24 17:12:25 +0500

    Firbase Push Notification Implemented

--- App.tsx
+++ App.tsx
@@ -27,7 +27,6 @@
 
 // // export default App;
 
-
 // import { StyleSheet, Text, View, PermissionsAndroid } from 'react-native'
 // import React from 'react'
 //   import { useEffect } from 'react';
@@ -36,7 +35,6 @@
 
 // const App = () => {
 //   // useNotification();
-
 
 // const requestPermission = async () => {
 //   const granted = await PermissionsAndroid.request(
@@ -65,7 +63,7 @@
 //   }, []);
 
 //   return (
-//     <View style={{alignItems:"center", justifyContent:"center",flex:1}}> 
+//     <View style={{alignItems:"center", justifyContent:"center",flex:1}}>
 //       <Text >FireBase Push Notifications</Text>
 //     </View>
 //   )
@@ -75,20 +73,48 @@
 
 // const styles = StyleSheet.create({})
 
-
-
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { PermissionsAndroid, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
 import Login from './src/screens/Login';
 import SignUp from './src/screens/SignUp';
 import MyStack from './src/Navigation/MyStack';
+import messaging from '@react-native-firebase/messaging';
 
 const App = () => {
-  return (
-    <MyStack  />
-  )
-}
+  const requestPermission = async () => {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
+    );
+    if (granted == PermissionsAndroid.RESULTS.GRANTED) {
+      console.log('Permission granted ');
+    } else {
+      console.log('Permission Denied');
+    }
+  };
+  // const getToken = async () => {
+  //   try {
+  //     const token = await messaging().getToken();
+  //     console.log('Token', token);
+  //   } catch (error) {
+  //     console.log('Error getting Token');
+  //   }
+  // };
+  const getToken = async () => {
+  try {
+    const token = await messaging().getToken();
+    console.log('Token:', token);
+  } catch (error) {
+    console.log('Error getting Token:', error.message);
+  }
+};
+
+  useEffect(() => {
+    requestPermission();
+    getToken();
+  }, []);
+  return <MyStack />;
+};
 
 export default App;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
